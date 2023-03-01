74002495605603edbc27bb9cbb823ba3e1c03e3c
Kevin Muscara <muscarak1@nku.edu>
2023-02-28 23:05:04 -0500

    Added findFirst function

--- src/index.js
+++ src/index.js
@@ -42,7 +42,35 @@ class Database {
   
           resolve({ status: 200, users: rows.map(([email, password]) => { return { email, password }}) });
         } catch(err) {
-          reject({ status: 400, error: 'error in findMany()'});
+          reject({ status: 400, error: `error in 'findMany()'`});
+        }
+      });
+    });
+  }
+
+  findFirst = (options = {}) => {
+    console.assert(options, `'options' parameter required.`);
+
+    return new Promise(async(resolve, reject) => {
+      await authorize().then(async(auth) => {
+        try {
+          const sheets    = google.sheets({ version: 'v4', auth });
+          const tableData = await sheets.spreadsheets.values.get({ spreadsheetId: this.spreadsheetId, range: 'Users!A2:B' });
+          const rows      = tableData.data.values;
+        
+          if(!rows || rows.length === 0)
+            resolve({status: 200, users: [] });
+
+          let users = rows.map(([email, password]) => { return { email, password }});
+          let user = users.filter(function(obj) {
+            return Object.keys(options).every(function(prop) {
+              return obj[prop] === options[prop];
+            });
+          })[0];
+
+          resolve({ status: 200, user });
+        } catch(err) {
+          reject({ status: 400, error: `error in 'findFirst()'`});
         }
       });
     });
--- src/test/_index.js
+++ src/test/_index.js
@@ -3,3 +3,5 @@ const Database = require('../index');
 const db = new Database('1gnnHyD7tLAJJsa4AunvokC6qbjNvRAK7vFAdzBiQX6Y');
 
 db.findMany().then(res => console.log(res));
+db.findFirst({ email: 'muscarak1@nku.edu' }).then(res => console.log(res));
+db.findFirst({ password: '123' }).then(res => console.log(res));
